03ad414b4d8dc800ff460651bc264a99ca698927
Isaac Lavender <musicman241504@gmail.com>
2023-10-04 17:47:02 +0100

    changed for loop condition

--- src/primes_java/Main.java
+++ src/primes_java/Main.java
@@ -5,11 +5,40 @@ import java.util.LinkedList;
 public class Main {
 
 	public static void main(String[] args) {
+//		long startTime = System.nanoTime();
+//		
+//		/* LinkedList<Integer> primes =  */linkedListPrimes(20000);
+//		//int[] primes = arrayPrimes(20000);
+//		
+//		long endTime = System.nanoTime();
+//		long time = endTime-startTime;
+//		System.out.println(time/1000000.0 + " ms");
+//		
+		
+		
+		
+		
+		long startTime = System.nanoTime();
+		
+		/*int[] primes = */arrayPrimes(10000000);
+		
+		long endTime = System.nanoTime();
+		long time = endTime-startTime;
+		System.out.println(time/1000000.0 + " ms");
+		
+
+//		for (int prime: primes) {
+//			System.out.println(prime);
+//		}
+
+	}
+	
+	public static LinkedList<Integer> linkedListPrimes(int max) {
 		boolean isPrime = true;
 		LinkedList<Integer> primes = new LinkedList<Integer>();
 		int num = 2;
-		primes.add(num);
-		while (num<1000) {
+		int i = 0;
+		while (i<max) {
 			isPrime = true;
 			for (int prime : primes) {
 				if (num%prime == 0) {
@@ -19,16 +48,40 @@ public class Main {
 			}
 			if (isPrime) {
 				primes.add(num);
+				i++;
 				
 			}
 			num++;
 		}
+		return primes;
 
-
-
-		for (int prime: primes) {
-			System.out.println(prime);
+	}
+	
+	
+	
+	public static int[] arrayPrimes(int max) {
+		boolean isPrime = true;
+		int[] primes = new int[max];
+		int num = 2;
+		int i = 0;
+		primes[0] = 2;
+		while (i<max) {
+			isPrime = true;
+			for (int j=0; primes[j]<=num/primes[j]; j++) {
+				if (num%primes[j] == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				//System.out.println(num);
+				primes[i] = num;
+				i++;
+				
+			}
+			num++;
 		}
+		return primes;
 
 	}
 
